5917e56b5213ac9a99a5756bef52d4ef11df8365
anitahasa22 <anita.hasan@g.bracu.ac.bd>
2021-08-21 20:29:41 +0600

    netw

--- js/script.js
+++ js/script.js
@@ -19,14 +19,14 @@ function macCost() {
     document.getElementById('apply-button').disabled = false;
 
 };
-//functions of buttons results
+//functions of button function results
 function updateTotalText() {
     const Msg = document.getElementById('promo-input');
     if (Msg.value.toLowerCase() == 'stevekaku') {
-        const total = document.getElementById('total-price');
+        //const total = document.getElementById('total-price');
         const bonus = document.getElementById('total-main-balance');
-        total.innerText = parseInt(total.innerText) * 0.8;
-        bonus.innerText = parseInt(total.innerText) * 0.8;
+        //total.innerText = parseInt(total.innerText) * 0.8;
+        bonus.innerText = parseInt(bonus.innerText) * 0.8;
         document.getElementById('apply-button').disabled = true;
 
     }
